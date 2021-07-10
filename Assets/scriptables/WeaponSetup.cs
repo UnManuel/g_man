c4a54803300b742b84cfd6f1f3ba72f528e43572
UnMAnU <m.herrera@getaclub.io>
2021-07-10 05:58:58 -0500

    Comments added to scriptable object

--- Assets/scriptables/WeaponSetup.cs
+++ Assets/scriptables/WeaponSetup.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "weapon_setup_", menuName = "Scriptables/WeaponSetup", order = 1)]
 public class WeaponSetup : ScriptableObject {
 	
-	public int bulletCount = 1;
-	public int bucketSize = 1;
-	public float maxCooldown = 0.5f;
+	public int bulletCount = 1;     	// Bullet rounds.
+	public int bucketSize = 1;      	// Number of bullet clones to be internally created.
+	public float maxCooldown = 0.5f;	// Duration between shots
 }
